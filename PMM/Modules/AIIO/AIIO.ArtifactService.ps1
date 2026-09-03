72f53ae18827dfc7a567756fa9e72d9284801f33
laredson <131942306+laredson@users.noreply.github.com>
2026-09-03 00:43:31 -0600

    Load AIIO preview 3 runtime fixes

--- PMM/Modules/AIIO/AIIO.ArtifactService.ps1
+++ PMM/Modules/AIIO/AIIO.ArtifactService.ps1
@@ -118,3 +118,4 @@ function Remove-PMMDisposableArtifacts {
 . (Join-Path $Script:Root 'Modules\AIIO\AIIO.CaseWorkspaceService.ps1')
 . (Join-Path $Script:Root 'Modules\AIIO\AIIO.CaseWorkspace.UI.ps1')
 . (Join-Path $Script:Root 'Modules\AIIO\AIIO.CaseWorkspace.UI.Preview2.ps1')
+. (Join-Path $Script:Root 'Modules\AIIO\AIIO.CaseWorkspace.UI.Preview3.ps1')
